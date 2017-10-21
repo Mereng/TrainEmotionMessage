5d90429195ee97b8fa0af372e30ca74a938a76ed
Mereng <vladislav-sedov@yandex.ru>
2017-10-21 10:03:26 +0300

    Закончил написание

--- train.py
+++ train.py
@@ -3,7 +3,11 @@ from nltk.tokenize import TweetTokenizer
 from collections import Counter
 from tflearn.data_utils import to_categorical
 from sklearn.model_selection import train_test_split
-import re, numpy, tensorflow, tflearn
+import re
+import numpy
+import tensorflow
+import tflearn
+import argparse
 
 
 class TweetTrain:
@@ -42,9 +46,25 @@ class TweetTrain:
                 stem = self.get_stem(token)
                 self.stem_count[stem] += 1
 
-    def create_vocabulary(self):
+    def create_vocabulary(self, positive_file, negative_file, save=False):
+        self.positive_tweets = open(positive_file).read().split('\n')
+        self.negative_tweets = open(negative_file).read().split('\n')
+
+        print('Считаем количество уникальных слов...')
+        self.count_unique_stem(self.positive_tweets)
+        self.count_unique_stem(self.negative_tweets)
+        print('Количетство уникальных слов:', len(self.stem_count))
+
+        print('Создаем словарь из', self.vocabulary_size, 'слов...')
+
         vocabulary_sort = sorted(self.stem_count, key=self.stem_count.get, reverse=True)[:self.vocabulary_size]
+        if save:
+            with open('data/vocabulary.txt', 'w') as f:
+                for i in vocabulary_sort:
+                    f.write(i + '\n')
+            print('Словарь сохранен data/vocabulary.txt')
         self.vocabulary = {vocabulary_sort[i] : i for i in range(self.vocabulary_size)}
+        print('Словарь создан')
 
     def tweet_to_vector(self, tweet):
         vector = numpy.zeros(self.vocabulary_size, dtype=numpy.byte)
@@ -58,19 +78,7 @@ class TweetTrain:
                 print("Не известное слово", token)
         return vector
 
-    def prepare_data(self, positive_file, negative_file):
-        self.positive_tweets = open(positive_file).read().split('\n')
-        self.negative_tweets = open(negative_file).read().split('\n')
-
-        print('Считаем количество уникальных слов...')
-        self.count_unique_stem(self.positive_tweets)
-        self.count_unique_stem(self.negative_tweets)
-        print('Количетство уникальных слов:', len(self.stem_count))
-
-        print('Создаем словарь из', self.vocabulary_size, 'слов...')
-        self.create_vocabulary()
-        print('Словарь создан')
-
+    def prepare_data(self):
         self.tweets_vectors = numpy.zeros((len(self.negative_tweets) + len(self.positive_tweets), self.vocabulary_size),
                                      dtype=numpy.byte)
         print('Создаем вектор из негативных постов')
@@ -82,6 +90,7 @@ class TweetTrain:
             self.tweets_vectors[i + len(self.negative_tweets)] = self.tweet_to_vector(tweet)
         self.labels = numpy.append(numpy.zeros(len(self.negative_tweets), dtype=numpy.byte),
                                    numpy.ones(len(self.positive_tweets), dtype=numpy.byte))
+        self.create_train_test_data()
         print('Подготовка завешена')
 
     def build_model(self, learning_rate=0.1):
@@ -97,24 +106,38 @@ class TweetTrain:
     def create_train_test_data(self):
         x = self.tweets_vectors
         y = to_categorical(self.labels, 2)
-        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, test_size=0.3)
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, test_size=0.1)
 
-    def train(self):
-        self.model = self.build_model(0.75)
-        self.model.fit(self.x_train, self.y_train, validation_set=0.1, show_metric=True, batch_size=128, n_epoch=30)
-        self.model.save('model.tflearn')
-
-    def load_model(self):
-        self.model = self.build_model(0.75)
-        self.model.load('model.tflearn')
+    def train(self, save_file=None):
+        with tensorflow.device('/gpu:0'):
+            self.model = self.build_model(0.75)
+            self.model.fit(self.x_train, self.y_train, validation_set=0.1, show_metric=True, batch_size=128, n_epoch=30)
+        if save_file is not None:
+            self.model.save(save_file)
 
     def test_model(self):
-        if self.model is None:
-            self.load_model()
+        print('Тестируем....')
         predictions = (numpy.array(self.model.predict(self.x_test))[:, 0] >= 0.5).astype(numpy.byte)
         accuracy = numpy.mean(predictions == self.y_test[:, 0], axis=0)
         print("Точность:", accuracy)
 
+
 if __name__ == '__main__':
-    foo = TweetTrain()
-    foo.prepare_data('data/positive_clear.txt', 'data/negative_clear.txt')
+    argparser = argparse.ArgumentParser(description='Обучалка tenserflow на распознавание эмоциональной окраски')
+    argparser.add_argument('--negative', action='store', type=str)
+    argparser.add_argument('--positive', action='store', type=str)
+    argparser.add_argument('--action', action='store', type=str)
+    argparser.add_argument('--test', default=False, action='store', type=bool)
+    argparser.add_argument('--debug', default=False, action='store', type=bool)
+    argparser.add_argument('--save-file', default=None, action='store', type=str)
+    args = vars(argparser.parse_args())
+
+    train = TweetTrain(debug=args['debug'])
+    if args['action'] == 'train':
+        train.create_vocabulary(args['positive'], args['negative'])
+        train.prepare_data()
+        train.train(args['save_file'])
+        if args['test']:
+            train.test_model()
+    elif args['action'] == 'create_vocabulary':
+        train.create_vocabulary(args['positive'], args['negative'], True)
